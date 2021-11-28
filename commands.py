7cd19419f84e83bb0131678543384934a0e76fec
codephunk <info@codephunk.de>
2021-11-28 10:00:58 +0100

    price channel name update now also posts current price into channel

---
+++ commands.py
@@ -0,0 +1,80 @@
+import discord
+from blockchain import *
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+
+async def display_price(ctx):
+    price_request_7 = get_price_x_days_ago(config["TOKEN_ADDRESS"], 7)
+    price_request_30 = get_price_x_days_ago(config["TOKEN_ADDRESS"], 30)
+    price_request = get_price(config["TOKEN_ADDRESS"])
+    native_price_request = get_price('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c')
+    try:
+        print("today: " + price_request['message'])
+        print("5D: " + price_request_7['message'])
+        print("30D: " + price_request_30['message'])
+        print("BNB: " + native_price_request["usdPrice"])
+        await ctx.send(price_request['message'])
+        return
+    except KeyError:
+        native_price_usd = native_price_request["usdPrice"]
+        token_wbnb_price = int(price_request["nativePrice"]["value"]) / (10 ** 18)
+        token_usd_price = token_wbnb_price * native_price_usd
+        movement_percent_7 = get_movement_percent(token_usd_price, price_request_7["usdPrice"])
+        movement_percent_30 = get_movement_percent(token_usd_price, price_request_30["usdPrice"])
+        price_usd = '{:.10f}'.format(token_usd_price) + ' USD'
+        embed = discord.Embed(title=price_usd,
+                              color=discord.Color.orange())
+        embed.add_field(name="**{:.2f}%**".format(movement_percent_7), value="7-day", inline=True)
+        embed.add_field(name="**{:.2f}%**".format(movement_percent_30), value="30-day", inline=True)
+        embed.set_author(name=config["TOKEN_SYMBOL"], url=config["TOKEN_URL"], icon_url=config["TOKEN_IMAGE"])
+        embed.set_footer(text="via %s" % price_request["exchangeName"])
+        await ctx.send(embed=embed)
+
+
+async def test_display_price(ctx):
+    price_request_7 = get_price_x_days_ago(config["TOKEN_ADDRESS"], 7)
+    price_request_30 = get_price_x_days_ago(config["TOKEN_ADDRESS"], 30)
+    price_request = get_price(config["TOKEN_ADDRESS"])
+    try:
+        print("today: " + price_request['message'])
+        print("5D: " + price_request_7['message'])
+        print("30D: " + price_request_30['message'])
+        await ctx.send(price_request['message'])
+        return
+    except KeyError:
+        token_usd_price = native_to_usd_price(price_request["nativePrice"]["value"])
+        movement_percent_7 = get_movement_percent(token_usd_price, price_request_7["usdPrice"])
+        movement_percent_30 = get_movement_percent(token_usd_price, price_request_30["usdPrice"])
+        price_usd = '{:.10f}'.format(token_usd_price) + ' USD'
+        embed = discord.Embed(title=price_usd, color=discord.Color.orange())
+        embed.add_field(name="**{:.2f}%**".format(movement_percent_7), value="7-day", inline=True)
+        embed.add_field(name="**{:.2f}%**".format(movement_percent_30), value="30-day", inline=True)
+        embed.set_author(name=config["TOKEN_SYMBOL"], url=config["TOKEN_URL"], icon_url=config["TOKEN_IMAGE"])
+        embed.set_footer(text="via %s" % price_request["exchangeName"])
+        await ctx.send(embed=embed)
+
+
+async def display_buy(ctx):
+    embed = discord.Embed(title="Buy %s on PancakeSwap" % config["TOKEN_SYMBOL"],
+                          url=config["BUY_URL"],
+                          color=discord.Color.orange())
+    await ctx.send(embed=embed)
+
+
+async def display_chart(ctx):
+    embed = discord.Embed(title="%s/USD Chart" % config["TOKEN_SYMBOL"],
+                          url=config["CHART_URL"],
+                          color=discord.Color.orange())
+    await ctx.send(embed=embed)
+
+
+async def display_contract(ctx):
+    embed = discord.Embed(title="%s Contract Address" % config["TOKEN_SYMBOL"],
+                          description=config["TOKEN_ADDRESS"],
+                          color=discord.Color.orange())
+    await ctx.send(embed=embed)
+
+
+async def rename_channel(channel, new_name):
+    await channel.edit(name=new_name)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import discord
 from json.decoder import JSONDecodeError
 from discord.ext import tasks
-from blockchain import *
+from commands import *
 
 config = dotenv_values(".env")
 client = discord.Client()
@@ -25,109 +24,38 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    """
-    Command: p
-    
-    Display price data for token.
-    """
+    # Command: p
+    #
+    # Display price data for token.
     if message.content.lower() == 'p':
-        ctx = message.channel
-        price_request_7 = get_price_x_days_ago(config["TOKEN_ADDRESS"], 7)
-        price_request_30 = get_price_x_days_ago(config["TOKEN_ADDRESS"], 30)
-        price_request = get_price(config["TOKEN_ADDRESS"])
-        native_price_request = get_price('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c')
-        try:
-            print("today: " + price_request['message'])
-            print("5D: " + price_request_7['message'])
-            print("30D: " + price_request_30['message'])
-            print("BNB: " + native_price_request["usdPrice"])
-            await ctx.send(price_request['message'])
-            return
-        except KeyError:
-            native_price_usd = native_price_request["usdPrice"]
-            token_wbnb_price = int(price_request["nativePrice"]["value"]) / (10 ** 18)
-            token_usd_price = token_wbnb_price * native_price_usd
-            movement_percent_7 = get_movement_percent(token_usd_price, price_request_7["usdPrice"])
-            movement_percent_30 = get_movement_percent(token_usd_price, price_request_30["usdPrice"])
-            price_usd = '{:.10f}'.format(token_usd_price) + ' USD'
-            embed = discord.Embed(title=price_usd,
-                                  color=discord.Color.orange())
-            embed.add_field(name="**{:.2f}%**".format(movement_percent_7), value="7-day", inline=True)
-            embed.add_field(name="**{:.2f}%**".format(movement_percent_30), value="30-day", inline=True)
-            embed.set_author(name=config["TOKEN_SYMBOL"], url=config["TOKEN_URL"], icon_url=config["TOKEN_IMAGE"])
-            embed.set_footer(text="via %s" % price_request["exchangeName"])
-            await ctx.send(embed=embed)
+        await display_price(message.channel)
 
-    """
-    Command: chart
-
-    Display a link to a candle chart for the token.
-    """
+    # Command: chart
+    #
+    # Display a link to a candle chart for the token.
     if message.content.lower() == 'chart':
-        ctx = message.channel
-        embed = discord.Embed(title="%s/USD Chart" % config["TOKEN_SYMBOL"],
-                              url=config["CHART_URL"],
-                              color=discord.Color.orange())
-        await ctx.send(embed=embed)
+        await display_chart(message.channel)
 
-    """
-    Command: contract
+    # Command: contract
+    #
+    # Display the contract address of the token.
+    if message.content.lower() == 'contract':
+        await display_contract(message.channel)
 
-    Display the contract address of the token.
-    """
-    if message.content == 'contract' or message.content == 'Contract':
-        ctx = message.channel
-        embed = discord.Embed(title="%s Contract Address" % config["TOKEN_SYMBOL"],
-                              description=config["TOKEN_ADDRESS"],
-                              color=discord.Color.orange())
-        await ctx.send(embed=embed)
-
-    """
-    Command: buy
-
-    Display a link to a exchange where you can buy the token.
-    """
+    # Command: buy
+    #
+    # Display a link to a exchange where you can buy the token.
     if message.content.lower() == 'buy':
-        ctx = message.channel
-        embed = discord.Embed(title="Buy %s on PancakeSwap" % config["TOKEN_SYMBOL"],
-                              url=config["BUY_URL"],
-                              color=discord.Color.orange())
-        await ctx.send(embed=embed)
+        await display_buy(message.channel)
 
-    """
-    Command: test_p
-
-    Development version of the p command.
-    """
+    # Command: test_p
+    #
+    # Development version of the p command.
     if message.content.lower() == 'test_p':
-        ctx = message.channel
-        price_request_7 = get_price_x_days_ago(config["TOKEN_ADDRESS"], 7)
-        price_request_30 = get_price_x_days_ago(config["TOKEN_ADDRESS"], 30)
-        price_request = get_price(config["TOKEN_ADDRESS"])
-        try:
-            print("today: " + price_request['message'])
-            print("5D: " + price_request_7['message'])
-            print("30D: " + price_request_30['message'])
-            await ctx.send(price_request['message'])
-            return
-        except KeyError:
-            token_usd_price = native_to_usd_price(price_request["nativePrice"]["value"])
-            movement_percent_7 = get_movement_percent(token_usd_price, price_request_7["usdPrice"])
-            movement_percent_30 = get_movement_percent(token_usd_price, price_request_30["usdPrice"])
-            price_usd = '{:.10f}'.format(token_usd_price) + ' USD'
-            embed = discord.Embed(title=price_usd, color=discord.Color.orange())
-            embed.add_field(name="**{:.2f}%**".format(movement_percent_7), value="7-day", inline=True)
-            embed.add_field(name="**{:.2f}%**".format(movement_percent_30), value="30-day", inline=True)
-            embed.set_author(name=config["TOKEN_SYMBOL"], url=config["TOKEN_URL"], icon_url=config["TOKEN_IMAGE"])
-            embed.set_footer(text="via %s" % price_request["exchangeName"])
-            await ctx.send(embed=embed)
+        await test_display_price(message.channel)
 
 
-async def rename_channel(channel, new_name):
-    await channel.edit(name=new_name)
-
-
-def update_price_channel():
+async def update_price_channel():
     for channel in price_channels:
         if channel is not None:
             price_request = get_price(config["TOKEN_ADDRESS"])
@@ -138,7 +66,8 @@ def update_price_channel():
                 token_usd_price = native_to_usd_price(price_request["nativePrice"]["value"])
                 formatted_price = '{:.10f}'.format(token_usd_price).replace('.', '‸')
                 print(formatted_price)
-                return rename_channel(channel, formatted_price)
+                await rename_channel(channel, formatted_price)
+                await display_price(channel)
 
 
 """
